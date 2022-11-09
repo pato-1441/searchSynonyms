436bf2a574b21e780e2e4d5a00c11a7b20750a33
Patricio Albornoz <albornozpatricio2004@gmail.com>
2022-11-09 20:19:50 -0300

    configurando estilos de arranque

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const App=()=> {
+const App = () => {
   return (
-    <div className="min-h-screen w-full bg-stone-900 text-white">
-      <h1>Hola como estas?</h1>
+    <div className="flex items-center justify-center min-h-screen w-full bg-stone-900 text-white">
+        <h1 className="text-2xl">Search Synonyms App</h1>
     </div>
   );
-}
+};
 
 export default App;
