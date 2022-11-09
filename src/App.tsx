e8c3ef99c2bcdeab530d2655502e7709b1f5df69
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-11-09 18:51:42 -0300

    project synonyms initialized with create-tw(vite+react+tailwind)

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App=()=> {
+  return (
+    <div className="min-h-screen w-full bg-stone-900 text-white">
+      <h1>Hola como estas?</h1>
+    </div>
+  );
+}
+
+export default App;
