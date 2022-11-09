3b00bb6799ee1ba527fc0ad717bbe498242f7e7e
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-11-09 20:47:17 -0300

    add input

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const App = () => {
   return (
     <div className="flex items-center justify-center min-h-screen w-full bg-stone-900 text-white">
+      <div className="flex flex-col gap-4">
         <h1 className="text-2xl">Search Synonyms App</h1>
+        <input type="text" placeholder="Insert a word" />
+      </div>
     </div>
   );
 };
