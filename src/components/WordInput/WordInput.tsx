49aab36fba868eb787782b182c7fcf8be4babc2f
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-11-10 10:04:17 -0300

    fetching good, showing results, set max results to 10

--- src/components/WordInput/WordInput.tsx
+++ src/components/WordInput/WordInput.tsx
@@ -1,23 +1,52 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 type Props = {};
 
 const WordInput = (props: Props) => {
   const [word, setWord] = useState("");
 
+  const [synonyms, setSynonyms] = useState([]);
+  console.log(synonyms);
+
+  const getSynonyms = async (e: React.FormEvent, word: string) => {
+    e.preventDefault();
+    const response = await fetch(
+      `https://api.datamuse.com/words?rel_syn=${word}&max=15`
+    );
+    const synonyms = await response.json();
+    return setSynonyms(synonyms);
+  };
+
   return (
-    <div className="flex flex-col gap-4">
-      <label htmlFor="wordInput" className="text-2xl">
-        Search Synonyms App
-      </label>
-      <input
-        id="wordInput"
-        onChange={(e) => setWord(e.target.value)}
-        value={word}
-        type="text"
-        className="text-black px-2 py-1 rounded bg-gray-200 hover:bg-white focus:bg-white"
-        placeholder="Insert a word to search for synonyms"
-      />
+    <div>
+      <div>
+        <form
+          className="flex flex-col gap-4"
+          onSubmit={(e) => getSynonyms(e, word)}
+        >
+          <label htmlFor="wordInput" className="text-2xl">
+            Search Synonyms App
+          </label>
+          <div className="flex justify-between gap-2">
+            <input
+              id="wordInput"
+              onChange={(e) => setWord(e.target.value)}
+              value={word}
+              type="text"
+              className="text-black px-2 py-1 rounded bg-gray-200 hover:bg-white focus:bg-white"
+              placeholder="Insert a word to search for synonyms"
+            />
+            <button type="submit" className="px-2 py-1 rounded bg-gray-600">
+              Go
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="mt-4">
+        <ul>
+          {synonyms && synonyms.map((synonym) => <li>- {synonym.word}</li>)}
+        </ul>
+      </div>
     </div>
   );
 };
