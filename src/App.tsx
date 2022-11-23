48525c6a3bdafc678a9f1645d3f93e9fa539035b
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-11-22 22:46:44 -0300

    cfontSize change, api correction to throw more accurate results

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import WordInput from "./components/WordInput/WordInput";
 
 const App = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen w-full bg-stone-900 text-white">
+    <div className="flex items-center justify-center min-h-screen w-full bg-stone-900 text-white text-2xl">
       <WordInput />
     </div>
   );
--- src/components/WordInput/WordInput.tsx
+++ src/components/WordInput/WordInput.tsx
@@ -15,7 +15,7 @@ const WordInput = (props: Props) => {
   const getSynonyms = async (e: React.FormEvent, word: string) => {
     e.preventDefault();
     const response = await fetch(
-      `https://api.datamuse.com/words?rel_syn=${word}&max=15`
+      `https://api.datamuse.com/words?rel_jjb=${word}&max=15`
     );
     const synonyms = await response.json();
     setWordInput(word)
@@ -29,12 +29,12 @@ const WordInput = (props: Props) => {
           className="flex flex-col gap-4"
           onSubmit={(e) => getSynonyms(e, wordInput)}
         >
-          <label htmlFor="wordInput" className="text-2xl text-center">
+          <label htmlFor="wordInput" className="text-center font-semibold">
             Search Synonyms App
           </label>
           <div className="flex justify-between gap-2">
             <button onClick={()=>setWordInput('')} className="px-2 py-1 rounded bg-red-600">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="w-4 h-4">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="w-5 h-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
               </svg>
             </button>
@@ -44,17 +44,17 @@ const WordInput = (props: Props) => {
               value={wordInput}
               type="text"
               className="text-black px-2 py-1 rounded bg-gray-200 hover:bg-white focus:bg-white"
-              placeholder="Insert a word to search for synonyms"
+              placeholder="Water"
             />
-            <button type="submit" className="px-2 py-1 rounded bg-gray-600 font-semibold">
+            <button type="submit" className="px-2 py-1 rounded bg-gray-600">
               Go
             </button>
           </div>
         </form>
       </div>
       <div className="mt-4">
-        {synonyms.length === 0 && <h2>- Results: 0</h2>}
-        {synonyms.length > 0 && <h2 className="font-semibold">Results: {synonyms.length}</h2>}
+        {synonyms.length === 0 && <h2>Results: 0</h2>}
+        {synonyms.length > 0 && <h2 className="">Results: {synonyms.length}</h2>}
         {synonyms.length > 0 &&
           synonyms.map((synonym) => (
             <ul>
